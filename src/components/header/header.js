a969c57fe96dd9e54362031e35518bb0ad3f29fa
nikolaevv <starlei165@gmail.com>
2021-08-22 16:12:54 +0300

    feat: Add gihub link

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,8 @@ import {Link} from 'react-router-dom';
 import useStyles from './styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import {Container} from '@material-ui/core';
+import {Container, IconButton} from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import './header.css';
 import logo from './main-logo.png';
 
@@ -15,10 +16,14 @@ const Header = () => {
         <div className={classes.root}>
             <AppBar className={classes.appBar} position="static">
                 <Container>
-                    <Toolbar>
+                    <Toolbar className={classes.toolbar}>
                         <Link to="/">
                             <img className="logo" alt="logo" src={logo}/>
                         </Link>
+
+                        <a href="https://github.com/nikolaevv">
+                            <GitHubIcon fontSize="large" color="primary"/>
+                        </a>
                     </Toolbar>
                 </Container>
             </AppBar>
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -2,19 +2,24 @@ import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
+
+    },
+    toolbar: {
+        display: "flex",
+        justifyContent: "space-between"
     },
     appBar: {
-      background: '#2d3436 !important',
-      boxShadow: 'none !important',
-      paddingTop: theme.spacing(1),
-      paddingLeft: 0,
-      paddingBottom: theme.spacing(1),
+        background: '#2d3436 !important',
+        boxShadow: 'none !important',
+        paddingTop: theme.spacing(1),
+        paddingLeft: 0,
+        paddingBottom: theme.spacing(1),
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+        marginRight: theme.spacing(0),
     },
     title: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
 }));
 
